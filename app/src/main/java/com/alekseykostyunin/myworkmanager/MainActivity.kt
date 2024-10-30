c61882b63c6dc7946f0a1712b70e6088bb97f334
Aleksey Kostyunin <kostunin-ag@mail.ru>
2024-10-30 18:03:29 +0500

    Initial commit

---
+++ app/src/main/java/com/alekseykostyunin/myworkmanager/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.alekseykostyunin.myworkmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var startWorkButton: Button
+    private lateinit var cancelWorkButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startWorkButton = findViewById(R.id.startWorkButton)
+        cancelWorkButton = findViewById(R.id.cancelWorkButton)
+
+        startWorkButton.setOnClickListener {
+            val myWorkRequest: PeriodicWorkRequest =
+                PeriodicWorkRequestBuilder<MyWorker>(
+                    15, TimeUnit.MINUTES
+                ).build()
+            WorkManager.getInstance(this)
+                .enqueueUniquePeriodicWork(
+                    WORK_ID,
+                    ExistingPeriodicWorkPolicy.KEEP,
+                    myWorkRequest)
+        }
+
+        cancelWorkButton.setOnClickListener {
+            WorkManager.getInstance(this).
+                    cancelUniqueWork(WORK_ID)
+        }
+    }
+    companion object {
+        private const val WORK_ID = "MyUniqueWork"
+    }
+}
---
+++ app/src/main/java/com/alekseykostyunin/myworkmanager/MyWorker.kt
@@ -0,0 +1,61 @@
+package com.alekseykostyunin.myworkmanager
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import kotlinx.coroutines.delay
+
+class MyWorker(appContext: Context, workerParams: WorkerParameters):
+    CoroutineWorker(appContext, workerParams) {
+
+    private val channelId = "CHANNEL_ID"
+    private val notificationId = 1
+    private val notificationManager =
+        appContext.getSystemService(Context.NOTIFICATION_SERVICE) as
+                NotificationManager
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override suspend fun doWork(): Result {
+
+        for (i in 30 downTo 1){
+            val progress = "Work In Progress Countdown: $i"
+            setForeground(createForegroundInfo(progress))
+            // Emulate 10 second work
+            delay(1000)
+        }
+
+        return Result.success()
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createForegroundInfo(progress: String): ForegroundInfo {
+
+        val channel = NotificationChannel(
+            channelId,
+            "Foreground Service Channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+
+        // This PendingIntent can be used to cancel the worker
+        val intent = WorkManager.getInstance(applicationContext)
+            .createCancelPendingIntent(id)
+
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setContentTitle("My Work")
+            .setTicker("My Work")
+            .setContentText(progress)
+            .setOngoing(true)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .addAction(android.R.drawable.ic_delete, "Cancel", intent)
+            .build()
+
+        return ForegroundInfo(notificationId, notification)
+    }
+}
